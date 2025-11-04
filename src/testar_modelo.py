10344612909b10208d05d17e98b5ef26947b8e7e
Gustavo Oliveira da Motta <127642611+gusstavomotta@users.noreply.github.com>
2025-11-04 11:21:44 -0300

    Atualizado 11:20
    Atualizada a estrutura do código, organização das pastas e alterações pequenas de código e estrutura.

--- src/testar_modelo.py
+++ src/testar_modelo.py
@@ -6,7 +6,7 @@ from PIL import Image
 import os
 
 MODEL_WEIGHTS_PATH = "resultados/best_model.pt" 
-IMAGE_TO_TEST = "foto_real_teste.jpg" 
+IMAGE_TO_TEST = "imagem_teste/foto_real_teste.jpg" 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Usando dispositivo: {DEVICE}")
 
