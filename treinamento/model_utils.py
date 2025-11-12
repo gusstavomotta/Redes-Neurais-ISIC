d7a581d934cc0b56feb438406ca8cb80b51b460c
Gustavo Oliveira da Motta <127642611+gusstavomotta@users.noreply.github.com>
2025-11-12 17:34:24 -0300

    a

--- treinamento/model_utils.py
+++ treinamento/model_utils.py
@@ -3,6 +3,8 @@ from torchvision.models import resnet50, ResNet50_Weights
 import torchvision.transforms as T
 import sys
 from pathlib import Path
+import torch.nn as nn
+from torchvision.models import mobilenet_v3_large, MobileNet_V3_Large_Weights
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent 
 sys.path.append(str(PROJECT_ROOT))
@@ -10,8 +12,11 @@ sys.path.append(str(PROJECT_ROOT))
 from config import IMG_SIZE, NORM_MEAN, NORM_STD
 
 def build_model(device):
-    model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
-    model.fc = nn.Linear(model.fc.in_features, 1)
+    model = mobilenet_v3_large(weights=MobileNet_V3_Large_Weights.IMAGENET1K_V1)
+    
+    in_features = model.classifier[0].in_features
+    model.classifier[3] = nn.Linear(model.classifier[3].in_features, 1)
+
     model = model.to(device)
     return model
 
